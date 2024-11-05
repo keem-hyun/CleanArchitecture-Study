ae347f428ca1a7917b2d4286fd535900f00a0eb0
keem-hyun <hw813@naver.com>
2024-11-05 16:25:13 +0900

    Initial Commit

---
+++ CleanArchitectureTests/CleanArchitectureTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureTests.swift
+//  CleanArchitectureTests
+//
+//  Created by 김강현 on 11/5/24.
+//
+
+import Testing
+@testable import CleanArchitecture
+
+struct CleanArchitectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
